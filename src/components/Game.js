cd63948b22b81fd7ad9dcb5aa891e50c942823f6
Ramgopal Bhat <subramanyaramgopal@gmail.com>
2020-03-16 20:25:20 +0530

    added score comparison while saving

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,83 +1,83 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import Question from './Question';
-import { loadQuestions } from '../helpers/QuestionsHelper';
-import HUD from './HUD';
-import SaveScoreForm from './SaveScoreForm';
+import React, { useState, useEffect, useCallback } from "react";
+import { Link } from "react-router-dom";
+import Question from "./Question";
+import { loadQuestions } from "../helpers/QuestionsHelper";
+import HUD from "./HUD";
+import SaveScoreForm from "./SaveScoreForm";
 
-export default class Game extends Component {
+export default function Game({ history }) {
+  const [questions, setQuestions] = useState([]);
+  const [currentQuestion, setCurrentQuestion] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [score, setScore] = useState(0);
+  const [questionNumber, setQuestionNumber] = useState(0);
+  const [done, setDone] = useState(false);
 
-  state = {
-    questions: null,
-    currentQuestion: null,
-    loading: true,
-    score: 0,
-    questionNumber: 0,
-    done: false
-  }
+  useEffect(() => {
+    loadQuestions()
+      .then(questions => setQuestions(questions))
+      .catch(console.error);
+  }, []);
 
-  async componentDidMount() {
-    try {
-      const questions = await loadQuestions();
-      console.log(questions);
-      this.setState({
-        questions
-      }, () => {
-        this.changeQuestion();
-      });
-    } catch (error) {
-      console.error(error);
+  const changeQuestion = useCallback(
+    (bonus = 0) => {
+      if (questions.length === 0) {
+        setDone(true);
+        return setScore(score + bonus);
+      }
+
+      const randomQuestionIndex = Math.floor(Math.random() * questions.length);
+      const currentQuestion = questions[randomQuestionIndex];
+      const remainingQuestions = [...questions];
+      remainingQuestions.splice(randomQuestionIndex, 1);
+
+      setQuestions(remainingQuestions);
+      setCurrentQuestion(currentQuestion);
+      setLoading(false);
+      setScore(score + bonus);
+      setQuestionNumber(questionNumber + 1);
+    },
+    [
+      score,
+      questions,
+      questionNumber,
+      setScore,
+      setQuestions,
+      setCurrentQuestion,
+      setLoading,
+      setQuestionNumber
+    ]
+  );
+
+  useEffect(() => {
+    if (!currentQuestion && questions.length) {
+      changeQuestion();
     }
-  }
+  }, [currentQuestion, questions, changeQuestion]);
 
-  changeQuestion = (bonus = 0) => {
-    const { questions } = this.state;
+  const scoreSaved = () => {
+    history.push("/");
+  };
 
-    if (questions.length === 0) {
-      return this.setState(prevState => ({
-        done: true,
-        score: prevState.score + bonus
-      }));
-    }
-
-    const randomQuestionIndex = Math.floor(Math.random() * questions.length);
-
-    const currentQuestion = questions[randomQuestionIndex];
-
-    const remainingQuestions = [...questions];
-    remainingQuestions.splice(randomQuestionIndex, 1);
-
-    this.setState((prevState) => ({
-      questions: remainingQuestions,
-      currentQuestion,
-      loading: false,
-      score: prevState.score += bonus,
-      questionNumber: prevState.questionNumber + 1
-    }))
-  }
-
-  render() {
-    const { score, questionNumber, currentQuestion, loading, done } = this.state;
-
-    return (
-      <>
-        {loading && <div id="loader"></div>}
-        {!loading && !done && <HUD
-          score={score}
-          questionNumber={questionNumber}
-        />}
-        {!done && currentQuestion && (
-          <>
-            <Link className="back" to="/">Back</Link>
-            <Question
-              question={currentQuestion}
-              changeQuestion={this.changeQuestion}
-            />
-          </>
-        )}
-        {/* {!done && !loading && <p>NOT DONE</p>} */}
-        {done && <SaveScoreForm score={score} />}
-      </>
-    )
-  }
+  return (
+    <>
+      {loading && <div id="loader" />}
+      {!loading && !done && (
+        <HUD score={score} questionNumber={questionNumber} />
+      )}
+      {!done && currentQuestion && (
+        <>
+          <Link className="back" to="/">
+            Back
+          </Link>
+          <Question
+            question={currentQuestion}
+            changeQuestion={changeQuestion}
+          />
+        </>
+      )}
+      {/* {!done && !loading && <p>NOT DONE</p>} */}
+      {done && <SaveScoreForm score={score} scoreSaved={scoreSaved} />}
+    </>
+  );
 }
--- src/components/SaveScoreForm.js
+++ src/components/SaveScoreForm.js
@@ -1,32 +1,64 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useFirebase } from "./firebase/FirebaseContext";
 
-export default function SaveScoreForm({ score }) {
-  const [username, setUsername] = useState('');
+export default function SaveScoreForm({ score, scoreSaved }) {
+  const [username, setUsername] = useState("");
+  const [islowScore, setIsLowScore] = useState(false);
+  const firebase = useFirebase();
 
-  const saveHighScore = (e) => {
+  useEffect(() => {
+    firebase.scores().once("value", snapshot => {
+      const data = snapshot.val();
+      if (Object.keys(data).length > 4) {
+        const lowScore = Object.values(data).reduce((prev, curr) =>
+          prev.score < curr.score ? prev.score : curr.score
+        );
+        if (lowScore > score) {
+          setIsLowScore(true);
+        }
+      }
+    });
+  }, [firebase, score]);
+
+  const saveHighScore = e => {
     e.preventDefault();
     const record = {
       name: username,
       score
-    }
-  }
+    };
+
+    firebase.scores().push(record, () => {
+      console.log("score saved");
+      scoreSaved();
+    });
+  };
 
   return (
     <>
-      <h1>Score: {score}</h1>
-      <form onSubmit={saveHighScore}>
-        <input
-          type="text"
-          name="username"
-          id="username"
-          placeholder="your username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <button type="submit" className="btn" disabled={!username}>Save</button>
-      </form>
-      <Link className="back" to="/">Go Home</Link>
+      {islowScore ? (
+        <h1>Your score is less than top 5 users. Play again</h1>
+      ) : (
+        <>
+          <h1>Score: {score}</h1>
+          <form onSubmit={saveHighScore}>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              placeholder="your username"
+              value={username}
+              onChange={e => setUsername(e.target.value)}
+            />
+            <button type="submit" className="btn" disabled={!username}>
+              Save
+            </button>
+          </form>
+        </>
+      )}
+      <Link className="back" to="/">
+        Go Home
+      </Link>
     </>
-  )
+  );
 }
